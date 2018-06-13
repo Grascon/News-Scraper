aaa4c68f42fbeb9c952911968cdde3d6c9aa05f0
Grascon <gilberto.r.s@icloud.com>
2018-06-12 17:56:17 -0700

    Added mongodb code

--- server.js
+++ server.js
@@ -22,21 +22,30 @@ var app = express();
 // Use morgan logger for logging requests
 app.use(logger("dev"));
 // Use body-parser for handling form submissions
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-// Set mongoose to leverage built in JavaScript ES6 Promises
-// Connect to the Mongo DB
-mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+var databaseUri = "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect("mongodb://localhost/week18Populater");
+if (process.env.MONGODB_URI){
+  mongoose.connect(process.env.MONGODB_URI);
+} else{
+  mongoose.connect(databaseUri);
+}
 
+var db = mongoose.connection;
+
+db.on('error', function(err){
+  console.log('Mongoose Error: ', err);
+});
+
+db.once('open', function(){
+  console.log('Mongoose Error: ', err);
+});
 // Routes
 
 // A GET route for scraping the echoJS website
